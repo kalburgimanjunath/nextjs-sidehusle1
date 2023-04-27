0aedc4f2a67c35cd503da167a3bd056f4ddb4176
kalburgimanjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2023-04-27 18:40:51 +0530

    1 file modified

--- pages/home.js
+++ pages/home.js
@@ -1,35 +1,36 @@
 import { useEarthoOne } from '@eartho/one-client-react';
 
 export default function Home() {
-  const { isLoading, isConnected, error, user, connectWithPopup, logout } =
-    useEarthoOne();
+  // const { isLoading, isConnected, error, user, connectWithPopup, logout } =
+  //   useEarthoOne();
+  // useEffect(() => {});
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-  if (error) {
-    return <div>Oops... {error.message}</div>;
-  }
-  if (isConnected) {
-    return (
-      <div>
-        Hello {user.displayName}{' '}
-        {/* <button onClick={() => logout({ returnTo: window.location.origin })}>
-          Log out
-        </button> */}
-      </div>
-    );
-  } else {
-    return (
-      <button
-        className="btn btn-outline-success"
-        id="login"
-        onClick={() => connectWithPopup({ access_id: 'wAXNIrrIkProRJubXNta' })}
-      >
-        Log in
-      </button>
-    );
-  }
+  // if (isLoading) {
+  //   return <div>Loading...</div>;
+  // }
+  // if (error) {
+  //   return <div>Oops... {error.message}</div>;
+  // }
+  // if (isConnected) {
+  //   return (
+  //     <div>
+  //       Hello {user.displayName}
+  //       <button onClick={() => logout({ returnTo: window.location.origin })}>
+  //         Log out
+  //       </button>
+  //     </div>
+  //   );
+  // } else {
+  //   return (
+  //     <button
+  //       className="btn btn-outline-success"
+  //       id="login"
+  //       onClick={() => connectWithPopup({ access_id: 'wAXNIrrIkProRJubXNta' })}
+  //     >
+  //       Log in
+  //     </button>
+  //   );
+  // }
   return (
     <div>
       <Sidebar />
