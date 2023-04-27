970dc3e3eff7e0907ba9656f542bb0a999079788
kalburgimanjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2023-04-27 19:21:29 +0530

    1 file modified

--- pages/contribute/index.js
+++ pages/contribute/index.js
@@ -1,15 +1,21 @@
+import { Input } from '../../components/';
 export default function index() {
   return (
     <div>
-      <h4>Submit a Post to our community</h4>
+      <header>
+        <h4>Submit a Post to our community</h4>
+      </header>
       <div>
-        Form goes here
-        {/* title
-        short description
-        content goes here
-        tags
-        category */}
+        <Input label="title" />
+        <Input label="short description" />
+        <Input label="content goes here" />
+        <Input label="tags" />
+        <Input label="category" />
       </div>
+      <footer>
+        <button className="btn btn-primary">Submit</button>
+        <button className="btn btn-default">Cancel Post</button>
+      </footer>
     </div>
   );
 }
