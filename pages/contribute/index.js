b8d5ef1d3139a1b010ac9e0cf6db00e3281d2439
kalburgimanjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2023-04-28 21:23:38 +0530

    1 file modified

--- pages/contribute/index.js
+++ pages/contribute/index.js
@@ -1,5 +1,5 @@
 import { Input } from '../../components/';
-import { Formik } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 export default function index() {
   return (
     <div>
@@ -8,115 +8,8 @@ export default function index() {
       </header>
 
       <div>
-        {/* <Formik
-          initialValues={{ title: 'enter your title' }}
-          validate={(values) => {
-            const errors = {};
-            if (!values.title) {
-              errors.title = 'Required';
-            } else {
-              errors.title = 'Invalid title ';
-            }
-            if (!values.description) {
-              errors.description = 'Required';
-            } else {
-              errors.description = 'Invalid description ';
-            }
-            if (!values.content) {
-              errors.content = 'Required';
-            } else {
-              errors.content = 'Invalid content ';
-            }
-            if (!values.tags) {
-              errors.tags = 'Required';
-            } else {
-              errors.tags = 'Invalid tags ';
-            }
-            if (!values.category) {
-              errors.category = 'Required';
-            } else {
-              errors.category = 'Invalid category ';
-            }
-            return errors;
-          }}
-          onSubmit={(values, { setSubmitting }) => {
-            setTimeout(() => {
-              alert(JSON.stringify(values, null, 2));
-              setSubmitting(false);
-            }, 400);
-          }}
-        >
-          {({
-            values,
-            errors,
-            touched,
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            isSubmitting,
-          }) => (
-            <form onSubmit={handleSubmit}>
-              <input
-                type="text"
-                placeholder="title"
-                id="title"
-                name="title"
-                value={values.title}
-                className="form-control"
-                onChange={handleChange}
-              />
-              {errors.name}
-              <button
-                type="submit"
-                className="btn btn-primary"
-                disabled={isSubmitting}
-              >
-                Submit{isSubmitting}
-              </button>
-              {/* <Input
-                label="title"
-                name="title"
-                id="title"
-                onInputChange={handleChange}
-                value={values.title}
-                errors={errors}
-              />
-              <Input
-                label="short description"
-                name="description"
-                onInputChange={handleChange}
-                value={values.description}
-                errors={errors}
-              />
-              <Input
-                label="content goes here"
-                name="content"
-                onInputChange={handleChange}
-                value={values.content}
-                errors={errors}
-              />
-              <Input
-                label="tags"
-                name="tags"
-                onInputChange={handleChange}
-                value={values.tags}
-                errors={errors}
-              />
-              <Input
-                label="category"
-                name="category"
-                onInputChange={handleChange}
-                value={values.category}
-                errors={errors}
-              /> 
-              <footer>
-                <button className="btn btn-default">Cancel Post</button>
-              </footer>
-            </form>
-          )}
-        </Formik> */}
         <Formik
-          initialValues={{ title: '' }}
+          initialValues={{ title: 'hello world' }}
           validate={(values) => {
             const errors = {};
             if (!values.title) {
@@ -162,42 +55,36 @@ export default function index() {
             handleSubmit,
             isSubmitting,
           }) => (
-            <form onSubmit={handleSubmit}>
-              <Input
+            <Form>
+              <Field
                 type="text"
-                label="title"
                 name="title"
+                value={values.title}
                 onChange={handleChange}
-                errors={errors.title}
+                onBlur={handleBlur}
               />
-              <Input
-                label="short description"
+              <ErrorMessage name="title" component="div" />
+
+              <Field
+                type="text"
                 name="description"
-                onInputChange={handleChange}
                 value={values.description}
-                errors={errors.description}
               />
-              <Input
-                label="content goes here"
-                name="content"
-                onInputChange={handleChange}
-                value={values.content}
-                errors={errors.content}
-              />
-              <Input
-                label="tags"
-                name="tags"
-                onInputChange={handleChange}
-                value={values.tags}
-                errors={errors.tags}
-              />
-              <Input
-                label="category"
+              <ErrorMessage name="description" component="div" />
+
+              <Field type="text" name="content" value={values.content} />
+              <ErrorMessage name="content" component="div" />
+
+              <Field type="text" name="tags" value="tags" value={values.tags} />
+              <ErrorMessage name="tags" component="div" />
+
+              <Field
+                type="text"
                 name="category"
-                onInputChange={handleChange}
+                value="category"
                 value={values.category}
-                errors={errors.category}
               />
+              <ErrorMessage name="category" component="div" />
 
               <footer>
                 <button
@@ -209,7 +96,7 @@ export default function index() {
                 </button>
                 <button className="btn btn-default">Cancel Post</button>
               </footer>
-            </form>
+            </Form>
           )}
         </Formik>
       </div>
