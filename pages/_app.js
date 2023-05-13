7d7101108d81712fcdb76c0ac8ac79736cd5ef59
kalburgimanjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2023-05-13 11:54:19 +0530

    3 files modified

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Header, Footer, Sidebar } from '../components/';
-import store from '../redux/store';
+// import store from '../redux/store';
 import { SessionProvider } from 'next-auth/react';
 function MyApp({ Component, pageProps, session }) {
   return (
